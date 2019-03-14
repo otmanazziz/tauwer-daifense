237bdc3ecd556c471fcfa99e54bbd40b1221f5fb
p1706731 <julian.cautela@etu.univ-lyon1.fr>
2019-03-14 14:45:34 +0100

    mosntre.h

--- src/Monstre.h
+++ src/Monstre.h
@@ -1,10 +1,17 @@
 #ifndef MONSTRE
 #define MONSTRE
 
+#include "Vect.h"
 class Monstre{
 private:
+	Vect pos ; 
+	float vitesse ;
+	int entier ; 
+	int etape ;
 
 public:
+	void perdreVie(const int & n );
+	
 }
 
 #endif
