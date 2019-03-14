9efdfbb557ba38bce8ecc6b4d410d2d9be8fb0d7
p1709456 <otman.azziz@etu.univ-lyon1.fr>
2019-03-14 18:08:53 +0100

    Ajout du menu (pas terminé donc stop flame)

--- src/Monstre.h
+++ src/Monstre.h
@@ -13,18 +13,8 @@ private:
 public:
 	Monstre();
 	void perdreVie(const int &n);
-=======
-	Vect pos ; 
-	float vitesse ;
-	int vie ; 
-	int etape ;
-
-public:
-	
 	Monstre(const int & v , const float & vit , const Vect & p );
-	void perdreVie(const int & n );
->>>>>>> bf8618b9a587fc4cf283109e9e4aacf916325cf9
 	
-}
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "Base.h"
 #include "Couleur.h"
 #include "Bouton.h"
+#include "Menu.h"
 
 using namespace std;
 
