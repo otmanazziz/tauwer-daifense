4893d0a45e9cddc3e6eb18f2ed431ea106786eb3
p1706731 <julian.cautela@etu.univ-lyon1.fr>
2019-03-16 22:53:08 +0100

    chemin

--- src/Chemin.cpp
+++ src/Chemin.cpp
@@ -1,1 +1,13 @@
-#include "chemin.h"
+#include "Chemin.h"
+
+Chemin::Chemin(const vector<Vect> t ){
+		tabEtape = t ; 		
+}
+
+void Chemin::ajouterEtape(const Vect & v){
+	tabEtape.push_back(v);
+}
+
+Vect Chemin::prochaineEtape(const int & e ){
+	return tabEtape[e];
+}
--- src/Chemin.h
+++ src/Chemin.h
@@ -1,8 +1,14 @@
 #ifndef CHEMIN
 #define CHEMIN
-
+#include <vector>
+#include "Vect.h"
 class chemin{
-	
+	private : 
+		std::vector<Vect> tabEtape ; 
+	public:
+		Chemin(const vector<Vect>);
+		void ajouterEtape(const Vect & v);
+		Vect prochaineEtape(const int & e );
 		
 	
 }
