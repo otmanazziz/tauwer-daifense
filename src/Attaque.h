0544574ac60fc9e2fcac9b6a57efcc822c5ec417
samy <samy.benothman@univ-lyon1.fr>
2019-03-18 17:06:21 +0100

    Commentaire Sort et attaque

--- src/Attaque.h
+++ src/Attaque.h
@@ -9,17 +9,101 @@ private:
 	Effet effet;
 	int degats;
 public:
-	Attaque();	
+	/**
+		@brief Constructeur par défaut: Attaque
+		Permet de créer l'attaque par défaut
+		@code
+		Attaque a;
+		@endcode
+	*/	
+ 	Attaque();
+ 	/**
+ 		@brief Constructeur par copie: Attaque
+ 		Permet de créer par copie une nouvelle attaque.	
+ 		@param Classe Attaque
+ 		@code
+ 			Attaque a1(a);
+ 		@endcode	
+ 	*/	
 	Attaque(const Attaque &_Attaque);
+	/**
+		@brief Constructeur:Tour
+		Permet de créer le constructeur de l'attaque avec les données privées de la fonction
+		@param Entier, Classe Effet, Entier
+		@code
+			Attaque a2(1 , e1 , 1)
+		@endcode	
+
+	*/
 	Attaque(const int &_zone, const Effet &_effet, int &_degats);
+	/** 
+		@brief Destructeur
+		Permet de détruire l'attaque
+
+	*/
 	~Attaque();
+	/**
+		@brief Fonction:getZone
+		Permet de retourner l'entier représentant la zone d'attaque
+		@return Entier
+		@code
+			a2.getZone();
+		@endcode	
+
+
+	*/
 
 	int getZone();
+	/**
+		@brief Effet: getEffet
+		Permet de retourner l'effet de l'attaque
+		@return Effet
+		@code
+			a2.getEffet();
+		@endcode	
+
+
+	*/
 	Effet getEffet();
+	/**
+		@brief Fonction: getDegats
+		Permet de retourner l'entier des dégats
+		@return Entier
+		@code
+			a2.getDegats();
+		@endcode	
+	*/
 	int getDegats();
+	/**
+		@brief Procédure: setZone
+		Permet de modifier l'entier représentant la zone d'attaque
+		@param Entier
+		@code
+			a2.setZone(2);
+		@endcode	
+	*/
 
 	void setZone(const int &_zone);
+	/**
+		@brief Procédure: setEffet
+		Permet de modifier l'effet
+		@param Effet
+		@code
+			a2.setEffet(e1);
+		@endcode	
+
+	*/
 	void setEffet(const Effet &_effet);
+	/**
+		@brief Procédure: setDegats
+		Permet de modifier les dégats
+		@param Entier
+		@code
+			a2.setDegats(5);
+		@endcode	
+
+	*/
+
 	void setDegats(const int &_degats);
 
 };
--- src/Sort.h
+++ src/Sort.h
@@ -9,15 +9,83 @@ private:
 	std::string nom;
 	Attaque attaque;
 public:
+	/**
+		@brief Constructeur par défaut: Sort
+		Permet de créer le sort par défaut
+		@code
+		Sort s;
+		@endcode
+	*/	
 	Sort();
+	 	/**
+ 		@brief Constructeur par copie: Sort
+ 		Permet de créer par copie un nouveau sort	
+ 		@param Classe Sort
+ 		@code
+ 			Sort s1(s);
+ 		@endcode	
+ 	*/	
 	Sort(const Sort &_Sort);
+	/**
+		@brief Constructeur:Sort
+		Permet de créer le constructeur du sort avec les données privées de la fonction
+		@param Chaîne de caractères, Classe Attaque
+		@code
+			Sort s2("", a2)
+		@endcode	
+
+	*/
 	Sort(const std::string &_nom, const Attaque &_attaque);
-	~Sort();	
+		/** 
+		@brief Destructeur
+		Permet de détruire le sort
+
+	*/
+	~Sort();
+		/**
+		@brief Fonction:getNom
+		Permet de retourner la chaîne de caractères représentant le nom du sort
+		@return Chaîne de caractère
+		@code
+			s2.getNom();
+		@endcode	
+
+
+	*/
 
 	std::string getNom();
+		/**
+		@brief Sort:getAttaque
+		Permet de retourner l'attaque associé au sort
+		@return Sort
+		@code
+			s2.getAttaque();
+		@endcode	
+
+
+	*/
 	Attaque getAttaque();
+		/**
+		@brief Procédure: setNom
+		Permet de modifier la chaîne de caractère représentant le nom du sort
+		@param Chaîne de caractère
+		@code
+			s2.setZone("a");
+		@endcode	
+	*/
+
 
 	void setNom(const std::string &_nom);
+		/**
+		@brief Procédure: setAttaque
+		Permet de modifier l'attaque
+		@param Attaque
+		@code
+			s2.setEffet(a2);
+		@endcode	
+
+	*/
+
 	void setAttaque(const Attaque &_attaque);
 };
 
