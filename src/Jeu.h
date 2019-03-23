a053eca2db4007fef65c7fb03bbf5296a6d71481
p1706731 <julian.cautela@etu.univ-lyon1.fr>
2019-03-23 11:00:42 +0100

    Jeu

--- src/Jeu.h
+++ src/Jeu.h
@@ -5,14 +5,17 @@
 #include <string>
 #include <iostream>
 #include "fonction.h"
+#include "Menu.h"
 class Jeu{
 	private:
 		std::vector<std::string> tabLienCarte;
+		Menu principale;
+		Niveau  * niv;
 	public :
 	
 		Jeu();
 		void afficheCarteDispo();
-	
+		Niveau * getNiveau(); 
 };
 
 #endif
