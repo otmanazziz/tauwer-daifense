33e4d6359bd0481b0f8f2e4b35f213d8a4a29ff1
p1709456 <otman.azziz@etu.univ-lyon1.fr>
2019-03-25 19:47:10 +0100

    Ajout de fichiers

--- src/sdlJeu.cpp
+++ src/sdlJeu.cpp
@@ -114,9 +114,9 @@ sdlJeu::sdlJeu () {
     renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
 
     // IMAGES
-    monstre.loadFromFile("../images/monster.png",renderer);
-    base.loadFromFile("../images/base.png",renderer);
-	tour.loadFromFile("../images/tower.png",renderer);
+    monstre.loadFromFile("monster.png",renderer);
+    base.loadFromFile("base.png",renderer);
+	tour.loadFromFile("tower.png",renderer);
 
     // FONTS
     font = TTF_OpenFont("DejaVuSansCondensed.ttf",50);
