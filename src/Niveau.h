ecfc15f04af84da2d8270b5274e1f0cc4bd6c27b
samy <samy.benothman@univ-lyon1.fr>
2019-04-02 10:11:15 +0200

    Doxygen Niveau

--- src/Niveau.h
+++ src/Niveau.h
@@ -13,16 +13,85 @@ class Niveau{
 		unsigned int Or;
 		int score;
 	public :
-	
+	/**
+		@brief Constructeur par défaut: Niveau
+		Permet de créer le niveau par défaut
+		@code
+			Niveau n;
+		@endcode
+	*/	
 		Niveau();
+	/**
+ 		@brief Constructeur par copie: Niveau
+ 		Permet de créer par copie un nouveau niveau
+ 		@param Chaîne de caractère f
+ 		@code
+ 			Niveau n1(Chaîne de caractère f);
+ 		@endcode	
+ 	*/
 		Niveau(const std::string & f);
-
+	/**
+		@brief Fonction: getCarte
+		Permet de retourner la carte
+		@return Carte carte
+		@code
+			n2.getCarte();
+		@endcode	
+	*/
 		Carte getCarte();
+	/**
+		@brief Fonction: getOr
+		Permet de retourner le nombre d'or
+		@return Entier positif or
+		@code
+			n2.getOr();
+		@endcode	
+	*/	
 		unsigned int getOr();
+	/**
+		@brief Fonction: addrGetCarte
+		Permet de retourner le pointeur de carte
+		@return Carte carte
+		@code
+			n2->addrGetCarte();
+		@endcode	
+	*/	
 		Carte *addrGetCarte();
-		void maj();
+	/**
+		@brief Procédure: maj
+		Permet de faire la mise à jour de la carte
+		@param Entier delta
+		@code
+			n2.maj(delta);
+		@endcode	
+	*/
+
+		void maj(float delta);
+	/**
+		@brief Procédure: ajouterOr
+		Permet de gagner de l'or en tuant des ennemis
+		@param Entier positif o
+		@code
+			n2.ajouterOr(o);
+		@endcode	
+	*/	
 		void ajouterOr(unsigned int o);
+	/**
+		@brief Procédure: retirerOr
+		Permet de perdre de l'or en tuant des ennemis
+		@param Entier positif o
+		@code
+			n2.ajouterOr(o);
+		@endcode	
+	*/		
 		void retirerOr(unsigned int o);
+	/**
+		@brief Procédure: affiche
+		Permet d'afficher le niveau
+		@code
+			n2.affiche();
+		@endcode	
+	*/		
 		void affiche();
 
 };
